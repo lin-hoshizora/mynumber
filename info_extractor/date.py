36c33d4f91fd80166b263023fb926eefb2474e54
lin-hoshizora <ncy906302.eo04g@g2.nctu.edu.tw>
2021-09-22 12:17:17 +0900

    fix month last day 99 -> 28,29,30,31

--- info_extractor/date.py
+++ info_extractor/date.py
@@ -1,3 +1,6 @@
+from info_extractor.re_pattern import LAST_DAY
+
+
 ERA_OFFSET = {
   "m": 1867,
   "t": 1911,
@@ -7,12 +10,29 @@ ERA_OFFSET = {
   "w": 0,
 }
 
+MONTH_LAST_DAY = {
+    '01':'31',
+    '02':'28',
+    '03':'31',
+    '04':'30',
+    '05':'31',
+    '06':'30',
+    '07':'31',
+    '08':'31',
+    '09':'30',
+    '10':'31',
+    '11':'30',
+    '12':'31',
+}
+
+
 class Date:
   def __init__(self, year, month, date, era):
     self.m = month
     self.d = date
     self.y = str(int(year) + ERA_OFFSET[era])
     self.jpy = year if era != "w" else None
+    self.check_last_day()
 
   def western_str(self):
     date_str = self.y + self.m.zfill(2) + self.d.zfill(2)
@@ -32,3 +52,11 @@ class Date:
 
   def __str__(self):
     return self.western_str()
+
+  def check_last_day(self):
+    if str(self.d) == '99':
+      if str(self.m.zfill(2)) == '02' and int(self.y)%4 ==0:
+          self.d=29
+      else:
+          self.d = MONTH_LAST_DAY[str(self.m.zfill(2))]
+
--- ocr2/info_extractor/date.py
+++ ocr2/info_extractor/date.py
@@ -9,6 +9,23 @@ ERA_OFFSET = {
 }
 
 
+MONTH_LAST_DAY = {
+    '01':'31',
+    '02':'28',
+    '03':'31',
+    '04':'30',
+    '05':'31',
+    '06':'30',
+    '07':'31',
+    '08':'31',
+    '09':'30',
+    '10':'31',
+    '11':'30',
+    '12':'31',
+}
+
+
+
 class Date:
   """Date class to hold year, month, day, and can output date string.
 
@@ -44,6 +61,7 @@ class Date:
     self.d = date
     self.y = str(int(year) + ERA_OFFSET[era])
     self.jpy = year if era != "w" else None
+    self.check_last_day()
 
   def western_str(self):
     """Generates date string using western year."""
@@ -63,3 +81,10 @@ class Date:
 
   def __str__(self):
     return self.western_str()
+
+  def check_last_day(self):
+    if str(self.d) == '99':
+      if str(self.m.zfill(2)) == '02' and int(self.y)%4 ==0:
+          self.d=29
+      else:
+          self.d = MONTH_LAST_DAY[str(self.m.zfill(2))]
